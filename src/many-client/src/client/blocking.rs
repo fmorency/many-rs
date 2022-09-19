1710df17a507ced7c3e12e422243be6cd1979df3
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2022-09-19 08:42:54 -0400

    WIP

--- src/many-client/src/client/blocking.rs
+++ src/many-client/src/client/blocking.rs
@@ -1,8 +1,11 @@
+use crate::client::send_envelope as send_envelope_async;
+use coset::CoseSign1;
 use many_identity::{Address, Identity};
 use many_modules::base::Status;
 use many_protocol::{RequestMessage, ResponseMessage};
 use many_server::ManyError;
 use minicbor::Encode;
+use once_cell::sync::OnceCell;
 use reqwest::IntoUrl;
 
 use crate::ManyClient as AsyncClient;
@@ -12,6 +15,8 @@ pub struct ManyClient<I: Identity> {
     client: AsyncClient<I>,
 }
 
+static RUNTIME: OnceCell<tokio::runtime::Runtime> = OnceCell::new();
+
 pub fn block_on<F>(future: F) -> F::Output
 where
     F: std::future::Future,
@@ -19,10 +24,15 @@ where
     match tokio::runtime::Handle::try_current() {
         Ok(handle) => tokio::task::block_in_place(|| handle.block_on(future)),
         Err(_) => {
-            let runtime = tokio::runtime::Runtime::new().unwrap();
+            let runtime = RUNTIME.get_or_init(|| tokio::runtime::Runtime::new().unwrap());
+            // let runtime = tokio::runtime::Runtime::new().unwrap();
             runtime.block_on(future)
         }
     }
+}
+
+pub fn send_envelope<S: IntoUrl>(url: S, message: CoseSign1) -> Result<CoseSign1, ManyError> {
+    block_on(send_envelope_async(url, message))
 }
 
 impl<I: Identity> ManyClient<I> {
