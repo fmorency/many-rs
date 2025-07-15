9d3d611098619d7cb23a8f6ff1d20cb794f147b2
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-07-15 10:54:41 -0400

    chore: .

--- src/ledger/src/main.rs
+++ src/ledger/src/main.rs
@@ -105,8 +105,6 @@ enum SubCommand {
 
     /// Perform a token operation
     Token(tokens::CommandOpt),
-
-    // Events(events::EventsOpt),
 }
 
 #[derive(Parser)]
@@ -383,7 +381,6 @@ fn main() {
         }
         SubCommand::Multisig(opts) => multisig::multisig(client, opts),
         SubCommand::Token(opts) => tokens::tokens(client, opts),
-        // SubCommand::Events(opts) => events::events(client, opts),
     };
 
     if let Err(err) = result {
